a66ddcc7edb889cc906d4ebd8f0ae76d856734b2
Vidhun724 <vidhunapoorvan724@gmail.com>
2026-02-28 10:26:07 +0530

    Dashboard modifed on 28-02-26 mrng

--- AI_Security_Log_Intelligence_Platform/backend/app.py
+++ AI_Security_Log_Intelligence_Platform/backend/app.py
@@ -1,106 +1,97 @@
 import os
-import sqlite3
+import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for , session
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.secret_key = "mysecretkey123"
 
-# ---------------- DATABASE PATH FIX ----------------
-# find project root directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# database location
-DB_PATH = os.path.join(BASE_DIR, "database", "users.db")
-# ---------------------------------------------------
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12345",
+        database="ai_security_platform"
+    )
+    return conn
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-
-
-# ---------------- SIGNUP PAGE (GET) ----------------
 @app.route('/signup', methods=['GET'])
 def signup_page():
     return render_template("signup.html")
-# ---------------------------------------------------
 
 
-# ---------------- HANDLE SIGNUP (POST) --------------
 @app.route('/signup', methods=['POST'])
 def signup():
 
     email = request.form['email']
     password = request.form['password']
 
-    # hash password (security)
     hashed_password = generate_password_hash(password)
 
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = get_db_connection()
         cursor = conn.cursor()
 
         cursor.execute(
-            "INSERT INTO users (email, password) VALUES (?, ?)",
+            "INSERT INTO users (email, password) VALUES (%s, %s)",
             (email, hashed_password)
         )
 
         conn.commit()
+        cursor.close()
         conn.close()
 
         return redirect(url_for('login_page'))
 
-    except sqlite3.IntegrityError:
+    except mysql.connector.IntegrityError:
         return "<body style='background:black'><center><h1 style='color:red;'>Email already registered ❌</h1><hr></center></body>"
-# ---------------------------------------------------
 
 
-# ---------------- LOGIN PAGE ----------------
 @app.route('/login', methods=['GET'])
 def login_page():
     return render_template("login.html")
-# --------------------------------------------
 
 
-# ---------------- HANDLE LOGIN ----------------
 @app.route('/login', methods=['POST'])
 def login():
 
     email = request.form['email']
     password = request.form['password']
 
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT password FROM users WHERE email = ?", (email,))
+    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cursor.fetchone()
 
+    cursor.close()
     conn.close()
 
-    # verify password
-    if user and check_password_hash(user[0], password):
+    if user and check_password_hash(user['password'], password):
         session['user'] = email
         return redirect(url_for('Dashboard'))
     else:
         return "<body style='background:black'><center><h1 style='color:red;'>Invalid Email or Password ❌</h1><hr></center></body>"
-# ---------------------------------------------------
 
 
-# ---------------- UPLOAD PAGE ----------------
 @app.route('/Dashboard')
 def Dashboard():
     if 'user' not in session:
         return redirect(url_for('login_page'))
     
     return render_template("Dashboard.html")
-# ---------------------------------------------
 
 
-# ---------------- RUN SERVER ----------------
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-# ---------------------------------------------
-
- 
